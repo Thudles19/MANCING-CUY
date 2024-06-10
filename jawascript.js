dbad5e0c2a310db297b902f59418cb37dd83210f
Thudles19 <thudles19@gmail.com>
2024-06-10 09:05:49 +0700

    Add files via upload

---
+++ jawascript.js
@@ -0,0 +1,7 @@
+function jawaban(jawab) {
+    if (jawab === 'yuk') {
+        alert('Oke Aku Tunggu Di Simpang Murai.');
+    } else if (jawab === 'ndak') {
+        alert('Oke Gitu Ye, Belaer Ngawanek Ko Gik e.');
+    }
+}
